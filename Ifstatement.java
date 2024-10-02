56cf8d6eed6542bd8b889ca6617b564897985e2d
Rachna jat <jatrachna941@gmail.com>
2024-10-02 06:24:28 -0700

    If Statement

---
+++ Ifstatement.java
@@ -0,0 +1,84 @@
+public class Ifstatement {
+    public static void main(String[] args) 
+    {
+        //if statement is used to execute a peace of code based on a condition
+    
+
+        if(5>1) 
+         System.out.println("hello");  //hello
+         System.out.println("hello");  //hello 
+         System.out.println("hello");   //hello
+         System.out.println("hello");   //hello
+        
+        if(true)
+        System.out.println("When it get true it will print the msg");//When it get true it will print the msg 
+
+        if(false)
+         System.out.println("When it get false it will not print any msg");
+
+
+        if(10<5)
+        System.out.println("condition give false value that's why it will not print any msg");  
+    
+
+        if(10>11)
+         System.out.println("10 is greater");
+        else
+        System.out.println("11 is greater"); ///11 is greater
+    
+
+
+        if(10>10)
+        System.out.println("true");
+        else
+        System.out.println("false"); ///false
+
+        if(10==10)
+        System.out.println("true"); //true
+        else
+        System.out.println("false"); 
+
+        if(10!=10)
+        System.out.println("true"); 
+        else
+        System.out.println("false"); //false
+        
+
+        int a=2;
+        char b='2';
+        if(a==b)
+        System.out.println("both are equal");
+        else
+        System.out.println("not equal"); //not equal
+
+
+        int c='2';
+        char d='2';
+        if(c==d)
+        System.out.println("both are equal");// both are equal
+        else
+        System.out.println("not equal"); 
+
+
+        int e=2;
+        char f=2;
+        if(e==f)
+        System.out.println("both are equal");//both are equal
+        else
+        System.out.println("not equal"); 
+
+        int g=2;
+        if(g==2)
+        System.out.println("both are equal");//both are equal
+        else
+        System.out.println("not equal"); 
+
+        int h=2;
+        if(h=='2')
+        System.out.println("both are equal");
+        else
+        System.out.println("not equal"); //not equal
+
+    }
+    
+}
