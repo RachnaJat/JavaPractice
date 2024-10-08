6d8737d54221de106ee6127a4afcdb9bcc19e979
Rachna jat <jatrachna941@gmail.com>
2024-10-08 05:04:21 -0700

    Comparison operators

---
+++ Comparison_operators.java
@@ -0,0 +1,31 @@
+public class Comparison_operators {
+    public static void main(String[] args) {
+ /*       
+int a=9;
+char b ='9';
+
+System.out.println("a<b = "+(a<b));//true
+System.out.println("a<=b = "+(a<=b));//true
+System.out.println("a>b = "+(a>b));//false
+System.out.println("a>=b = "+(a>=b));//false
+System.out.println("a==b = "+(a==b));//false
+System.out.println("a!=b = "+(a!=b));//true
+
+ */
+int a='9';
+char b ='9';
+System.out.println(a);//57
+System.out.println(b);//9
+
+System.out.println("a<b = "+(a<b));//false
+System.out.println("a<=b = "+(a<=b));//true
+System.out.println("a>b = "+(a>b));//false
+System.out.println("a>=b = "+(a>=b));//true
+System.out.println("a==b = "+(a==b));//true
+System.out.println("a!=b = "+(a!=b));//false
+
+
+
+
+    }
+}
