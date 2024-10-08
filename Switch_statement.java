b71782f0781623ae787c698a2ec9375e46791b65
Rachna jat <jatrachna941@gmail.com>
2024-10-08 05:27:28 -0700

    Switch Statement

---
+++ Switch_statement.java
@@ -0,0 +1,71 @@
+public class Switch_statement {
+    public static void main(String[] args) {
+      
+/* 
+        int n =10;
+        switch(n)
+        {
+
+            case 1:
+            System.out.println("= 1");
+
+            case 20:
+            System.out.println("= 20");
+
+            case 10:
+            System.out.println("= 10"); //10
+            
+            case -2:
+            System.out.println("= -2");//-2
+
+            default:
+            System.out.println("none of the above");//none of the above
+
+        }
+ */
+
+
+
+
+ /*
+ 
+ int a =-2;
+ switch(10+10)       //switch(20)
+   switch(0)          // none of the above
+  switch(a)           //  = -2
+  switch(a+12)      // =  10
+ */
+
+
+ int b =0;
+switch(b*5)
+
+ {
+    default:
+    System.out.println("none of the above"); //none of the above
+ break;
+     case 1:
+     System.out.println("= 1");  
+     break;
+
+     case 20:
+     System.out.println("= 20");
+     break;
+
+     case 10:
+     System.out.println("= 10"); 
+     break;
+
+     case -2:
+     System.out.println("= -2");
+     break;
+
+  
+
+ }
+
+
+
+
+    }
+}
