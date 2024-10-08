356d872049ffac9e0c3238c5589b0a5bd1c42f06
Rachna jat <jatrachna941@gmail.com>
2024-10-08 04:43:21 -0700

    Assignment operators

---
+++ Assignment_operators.java
@@ -0,0 +1,21 @@
+public class Assignment_operators {
+    public static void main(String[] args) {
+
+        int a=4;
+        int b=a;
+        System.out.println("value of a :"+a);//4
+        System.out.println("value of b :"+b);//4
+
+        b+=3;
+        System.out.println("value of b = "+b);//7
+        System.out.println(b-=1);//6
+        System.out.println(b*=1);//6
+        System.out.println(b/=2);//3
+        System.out.println(b%=2);//1
+    
+      
+    }
+    
+}
+
+//Assignment operator in java,denoted by (=) use to assign a value to variable 
