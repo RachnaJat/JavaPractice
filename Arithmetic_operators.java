0af6b8b8a9f9adc2722532c377b0526277539c0e
Rachna jat <jatrachna941@gmail.com>
2024-10-08 04:09:11 -0700

    Arithmetic operators

---
+++ Arithmetic_operators.java
@@ -0,0 +1,34 @@
+public class Arithmetic_operators {
+    public static void main(String[] args)
+    
+    {
+        
+        int m =5;
+        int n =4*m;
+        System.out.println(n);
+
+
+        int x=20,y=10;
+        System.out.println("sum of 20+10 ="+x+y);//2010
+        System.out.println("sum of 20+10 ="+(x+y));//30
+        System.out.println("Substraction  of 20-10 ="+ (x-y));//10
+        System.out.println("Multiplication of 20*10 ="+x*y);//200
+        System.out.println("Division of 20/10 ="+x/y);//2
+        System.out.println("Modulus of 20%10 ="+x%y);//0
+
+
+
+        int a=20,b=30;
+        System.out.println("sum of 20+30 ="+a+b);//2030
+        System.out.println("sum of 20+30 ="+(a+b));//50
+        System.out.println("Substraction  of 20-30 ="+ (a-b));//-10
+        System.out.println("Multiplication of 20*30 ="+a*b);//600
+        System.out.println("Division of 20/30 ="+a/b);//0
+        System.out.println("Modulus of 20%30 ="+a%b);//20
+
+
+
+
+    }
+    
+}
