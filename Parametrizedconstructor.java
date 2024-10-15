17f7cc461ff0bbc07d62c69fd31bca6f884ea5bc
Rachna jat <jatrachna941@gmail.com>
2024-10-15 01:12:53 -0700

    Parametrized Constructor

---
+++ Parametrizedconstructor.java
@@ -0,0 +1,10 @@
+public class Parametrizedconstructor {
+    Parametrizedconstructor(String name)
+    {
+        System.out.println("name : "+name);
+    }
+    public static void main(String[] args) {
+        
+        Parametrizedconstructor  obj = new Parametrizedconstructor("Rachna Jat") ; 
+    }
+}
