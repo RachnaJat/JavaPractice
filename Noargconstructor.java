e839b44a5c805cda93a40aaba71bb489f5c566bd
Rachna jat <jatrachna941@gmail.com>
2024-10-15 01:05:06 -0700

    No argument constructor

---
+++ Noargconstructor.java
@@ -0,0 +1,15 @@
+//NO Argument Constructor
+
+public class Noargconstructor {
+   
+    Noargconstructor()
+    {
+        System.out.println("no argument constructor");
+    }
+    public static void main(String[] args) {
+       
+        Noargconstructor obj = new Noargconstructor();
+       // obj1.Noargconstructor(); //error
+    }
+    
+}
