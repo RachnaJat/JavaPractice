c52bda7f5dca07de01c34e509b4753a9fa203e08
Rachna jat <jatrachna941@gmail.com>
2024-10-15 00:58:12 -0700

    Default constructor

---
+++ Defaultconstructor.java
@@ -0,0 +1,12 @@
+public class Defaultconstructor
+ {
+    String  name;
+    public static void main(String[] args)
+    {
+     
+        Defaultconstructor obj = new Defaultconstructor();  
+        System.out.println(obj.name);//null
+
+    }
+}
+//default constructor is created by compiler
